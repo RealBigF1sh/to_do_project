e08708963d9b41bd153bb6ffa869ef2495405f2f
RealBigF1sh <tarans.fishborn@gmail.com>
2023-02-16 21:28:07 +0300

    Project and To_Do models created

--- todo/to_do/models.py
+++ todo/to_do/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(max_length=1024,null=True,blank=True)
+    creator = models.ManyToManyField(User)
+    repository = models.URLField()
+
+class To_Do(models.Model):
+    to_project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    description = models.TextField(max_length=1024,null=True,blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -43,8 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'to_do'
     'users',
     'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
