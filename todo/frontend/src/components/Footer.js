80bcb63842c5eaa6e89e8ceaebd9597180cad012
Konstantin Zaytsev <104307939+RealBigF1sh@users.noreply.github.com>
2023-02-16 20:49:41 +0300

    Lesson 2 (#1)
    * lesson_2 Done

---
+++ todo/frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Foo = ({foo}) => {
+    return(
+        <footer>
+            Direct by Zaytsev Konstantin a.k.a RealBigF1sh for gb.ru
+        </footer>
+
+    )
+}
+
+export default Foo
---
+++ todo/frontend/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainMenu = ({menu}) => {
+    return(
+        <ul>
+            <li><a href="">Все пользователи</a></li>
+            <li><a href="">Проекты</a></li>
+            <li><a href="">To Do</a></li>
+        </ul>
+    )
+}
+
+export default MainMenu
