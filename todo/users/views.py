1f970bfd90d15a35ec53f90e9c7c4c8bd59ae8e6
RealBigF1sh <tarans.fishborn@gmail.com>
2023-02-20 22:09:36 +0300

    CustomViewSet for Users created

--- todo/users/views.py
+++ todo/users/views.py
@@ -1,9 +1,10 @@
-from rest_framework.renderers import JSONRenderer
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework import mixins, viewsets
 from .models import User
 from .serializer import UserModelSerializer
 
-class UserModelViewSet(ModelViewSet):
+class UserCustomViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     
