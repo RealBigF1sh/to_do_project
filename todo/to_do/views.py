feee32d017023dcb3e2bd5457318ab2b44e27438
RealBigF1sh <tarans.fishborn@gmail.com>
2023-02-20 23:28:19 +0300

    Destroy func for To_Do's is create

--- todo/to_do/views.py
+++ todo/to_do/views.py
@@ -1,11 +1,13 @@
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import status
+from rest_framework.response import Response
 from to_do.models import Project, ToDo
 from to_do.serializers import ProjectModelSerializer, ToDoModelSerializer
 
 class ProjectLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 10
+    page_size = 10
 
 class ProjectFilterViewSet(ModelViewSet):
     queryset = Project.objects.all()
@@ -21,11 +23,20 @@ class ProjectFilterViewSet(ModelViewSet):
         return projects
 
 class ToDoLimitOffsetPagination(LimitOffsetPagination):
-    default_limit = 20
+    page_size = 20
 
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     pagination_class = ToDoLimitOffsetPagination
-    
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object
+            instance.is_active = False
+            instance.save()
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(status=status.HTTP_204_NO_CONTENT)
